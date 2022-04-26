45359f435a7a1a39c5c8e677240542ff570628e3
Al Camara <Alhousseinec@gmail.com>
2022-04-26 08:40:44 -0500

    create collection array & addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+let addToCollection = (title,artist,yearPublished) =>{
+ let newAlbum  = {
+        title,
+        artist,
+        yearPublished,
+    }
+
+    return collection.push(newAlbum);
+}
+
+
+addToCollection('Comming Home','sha',2022);
+
+console.log(collection);
